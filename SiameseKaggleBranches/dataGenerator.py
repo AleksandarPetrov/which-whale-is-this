55914300c6f80bb0539a23d426655c81d6e85956
jakub <kubajanowski@gmail.com>
2018-05-29 21:14:59 +0200

    Made the datagenerator work for array instead of single files.
    Made the according changes to main.

--- SiameseKaggleBranches/dataGenerator.py
+++ SiameseKaggleBranches/dataGenerator.py
@@ -22,23 +22,26 @@ from data_aug import img_data_aug_array, aug_para
 class SiameseDataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
 
-    def __init__(self, list_IDs, labels, parent_dir, batch_size=32, dim=(32, 32), n_channels=1,
-                 n_classes=10, shuffle=True):
+    def __init__(self, parent_dir,X_dataset,y_labels, batch_size=32, dim=(64, 64), n_channels=1, shuffle=True):
         'Initialization'
         self.dim = dim
         self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_IDs
         self.n_channels = n_channels
-        self.n_classes = n_classes
         self.shuffle = shuffle
-        self.on_epoch_end()
         self.shown = 0
         self.parent_dir = parent_dir
+        self.X_dataset = X_dataset
+        self.y_labels = y_labels
+        self.indexes = np.arange(0,np.size(y_labels,0))
+        self.augParam = aug_para(rot_deg=min(10, max(-10, np.random.normal(loc=0.0, scale=5))),
+                            width=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
+                            height=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
+                            shear=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
+                            zoom=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))))  
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
+        return int(np.floor(len(self.y_labels) / self.batch_size))
 
     def __getitem__(self, index):
         'Generate one batch of data'
@@ -46,75 +49,53 @@ class SiameseDataGenerator(keras.utils.Sequence):
         indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
         # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        y_labels = [self.y_labels[k] for k in indexes]
         # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
+        X, y = self.__data_generation(y_labels,indexes)
 
         return X, y
 
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
 
-    def __data_generation(self, list_IDs_temp):
+
+    def __data_generation(self, y_labels,indexes):
         'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
         X1 = np.empty((self.batch_size, *self.dim, self.n_channels))
         X2 = np.empty((self.batch_size, *self.dim, self.n_channels))
         y = np.empty((self.batch_size), dtype=int)
-
+        total_indexes = np.arange(0,np.size(self.y_labels))
+                
         # Generate data
-        for i, ID in enumerate(list_IDs_temp):
+        for i, label in enumerate(y_labels):
             # Store sample 1
-            img = np.load(os.path.join(self.parent_dir, 'train_npy/' + ID + '.npy'))
+            ind1 = indexes[i]
+            img = self.X_dataset[ind1]
             img = img[:, :, np.newaxis]
             X1[i,] = img
-            X1_label = self.labels[ID]
 
-            listOfPicturesOfSameWhale = [k for k in list(self.labels.keys()) if self.labels[k] == X1_label]
             # For the second one take one from the same class is i is even, otherwise one with a different class,
             # also checks if the class is not new_whale and if there is at least one other picture of the same whale
             ###DATA AUGMENTATION SHOULD BE PUT HERE AND A LOT OF THINGS ADJUSTED
-            if i%2==0 and X1_label!=0:
-                X2_ID = np.random.choice(listOfPicturesOfSameWhale, 1)[0]
-                img = np.load(os.path.join(self.parent_dir, 'train_npy/' + X2_ID + '.npy'))
+            if i%2==0:
+                positive_indexes = np.where(self.y_labels == label)                
+                ind2 = np.random.choice(positive_indexes,size=1)
+                img = self.X_dataset[ind2]
+                
                 # Augment:
-                augParam = aug_para(rot_deg=min(10, max(-10, np.random.normal(loc=0.0, scale=5))),
-                                    width=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
-                                    height=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
-                                    shear=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
-                                    zoom=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))))
-                img = img_data_aug_array(augParam, img)
-                #if(self.shown<30):
-                #    plt.imshow(img)
-                #    plt.savefig('foo'+str(self.shown)+'.png')
-                #    self.shown=self.shown+1
+                img = img_data_aug_array(self.augParam, img)
                 img = img[:, :, np.newaxis]
                 X2[i,] = img
                 # Store class
                 y[i] = 1
             else:
-                listOfPicturesOfDifferentWhales = [k for k in list(self.labels.keys()) if (self.labels[k] != X1_label or self.labels[k]!=0) and k != ID]
-                X2_ID = np.random.choice(listOfPicturesOfDifferentWhales, 1)[0]
-                img = np.load(os.path.join(self.parent_dir, 'train_npy/' + X2_ID + '.npy'))
+                negative_indexes = np.delete(total_indexes,positive_indexes)
+                ind2 = np.random.choice(negative_indexes,size=1)
+                img = self.X_dataset[ind2]
+    
                 # Augment:
-                augParam = aug_para(rot_deg=min(10, max(-10, np.random.normal(loc=0.0, scale=5))),
-                                    width=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
-                                    height=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
-                                    shear=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))),
-                                    zoom=min(0.1, max(-0.1, np.random.normal(loc=0.0, scale=0.05))))
-                img = img_data_aug_array(augParam, img)
-                #if (self.shown < 30):
-                #    plt.imshow(img)
-                #    plt.savefig('foo'+str(self.shown)+'.png')
-                #    self.shown=self.shown+1
+                img = img_data_aug_array(self.augParam, img)
                 img = img[:, :, np.newaxis]
                 X2[i,] = img
                 # Store class
                 y[i] = 0
-
-
-
         return [X1, X2], y
--- SiameseKaggleBranches/main.py
+++ SiameseKaggleBranches/main.py
@@ -1,17 +1,13 @@
 import numpy as np
-import keras
 from keras.layers import Input, Conv2D, Lambda, merge, Dense, Flatten, MaxPooling2D
 from keras.models import Model, Sequential, load_model
 from keras.regularizers import l2
 from keras import backend as K
 from keras.optimizers import SGD, Adam
 from keras.losses import binary_crossentropy
-import numpy.random as rng
-import sys
 import os
-import glob
+
 from sklearn.preprocessing import LabelEncoder
-from numpy import zeros
 import pandas as pd
 from keras.callbacks import ModelCheckpoint
 from gen_imageName_dict import gen_imageName_dict
@@ -20,9 +16,9 @@ import matplotlib.pyplot as plt
 from data_aug import img_data_aug_array, aug_para
 from dataGenerator import SiameseDataGenerator
 from architecture import basicSiameseGenerator
+import h5py
 
-
-TRAIN_TOP_N_WHALES = True
+TRAIN_TOP_N_WHALES = False
 N = 20
 N_EPOCHS = 15
 LOAD_WEIGHTS = True
@@ -31,77 +27,75 @@ LOAD_WEIGHTS = True
 if __name__ == "__main__":
 
     # Some useful directories
-    parent_dir = '../../DATA/'#sys.argv[1]
+    parent_dir = './DATA/'#sys.argv[1]
     test_dir = os.path.join(parent_dir, 'test_npy')
     train_dir = os.path.join(parent_dir, 'train_npy')
     labels_dir = os.path.join(parent_dir, 'train.csv')
-
+    
+    dataset = h5py.File('tr_gr_64.h5', 'r')
+    X_dataset = np.array(dataset['x'])
+    y_labels = np.array(dataset['y'])
+    y_labels = y_labels.astype('str')
     # Reading of labels and corresponding image names
-    classes = pd.read_csv(labels_dir)
-    list_ids = list(classes.Id)
-    file_names = list(classes.Image)
-    n_files = len(file_names)
-    file_names = [file[:-4] for file in file_names]
+#    classes = pd.read_csv(labels_dir)
+#    list_ids = list(classes.Id)
+#    file_names = list(classes.Image)
+#    n_files = len(file_names)
+#    file_names = [file[:-4] for file in file_names]
+#
+#    # Label encoder, changes the label names to integers for use in generator
+#    le = LabelEncoder()
+#    le.fit(list_ids)
+#    n_classes = len(le.classes_)
+#    labels_int = list(le.fit_transform(list_ids))
+#
+#    if TRAIN_TOP_N_WHALES:
+#        # Count
+#        whale_counts = Counter(list_ids)
+#        whale_counts_most_data = whale_counts.most_common(N)  # whale IDs for n most common whales
+#        number_images = sum([element[1] for element in whale_counts_most_data])
+#        whale_IDs_most_data = [element[0] for element in whale_counts_most_data]  # get the whale_Ids only
+#
+#        # get indexes of these whales in the entire training dataset
+#        list_ids_arr = np.array(list_ids)
+#        idx_whale_IDs_most_data = []
+#        for i in range(len(whale_IDs_most_data)):
+#            indexes = np.where(list_ids_arr == whale_IDs_most_data[i])[0]
+#            idx_whale_IDs_most_data.extend(list(indexes))
+#        idx_whale_IDs_most_data.sort()
+#
+#        # find image names corresponding to these whales
+#        file_names_sub = [file_names[i] for i in idx_whale_IDs_most_data]
+#        labels_int_sub = [labels_int[i] for i in idx_whale_IDs_most_data]
+#        # should this be refit??
+#        labels_int_sub_refit = list(le.fit_transform(labels_int_sub))  # refit it to the N classes
+#
+#        # labels_sub = [list_ids[i] for i in idx_whale_IDs_most_data]
+#        # labels_sub == whale_IDs_most_data
+#
+#        # Parameters for Generator
+#        partition = gen_imageName_dict(test_dir, train_dir, 0.2, file_names_sub)
+#        imageName_ID_dict = dict(zip(file_names_sub, labels_int_sub_refit))
+#        n_classes = N
+#    else:
+#        # Parameters for Generator
+#        partition = gen_imageName_dict(test_dir, train_dir, 0.2)
+#        imageName_ID_dict = dict(zip(file_names, labels_int))
+#        n_classes = len(le.classes_)
+#
+#    # Parameters for Generator [OLD, BEFORE THE TRAIN TOP WHALES]
+#    #partition = gen_imageName_dict(test_dir, train_dir, validation_fraction=0.2)
+#    #labels = dict(zip(file_names, labels_int))
+#
+#    params = {'dim': (250, 500),
+#              'batch_size': 32,
+#              'n_classes': n_classes,
+#              'n_channels': 1,
+#              'shuffle': True}
 
-    # Label encoder, changes the label names to integers for use in generator
-    le = LabelEncoder()
-    le.fit(list_ids)
-    n_classes = len(le.classes_)
-    labels_int = list(le.fit_transform(list_ids))
 
-    if TRAIN_TOP_N_WHALES:
-        # Count
-        whale_counts = Counter(list_ids)
-        whale_counts_most_data = whale_counts.most_common(N)  # whale IDs for n most common whales
-        number_images = sum([element[1] for element in whale_counts_most_data])
-        whale_IDs_most_data = [element[0] for element in whale_counts_most_data]  # get the whale_Ids only
+    training_generator = SiameseDataGenerator(parent_dir,X_dataset,y_labels)
 
-        # get indexes of these whales in the entire training dataset
-        list_ids_arr = np.array(list_ids)
-        idx_whale_IDs_most_data = []
-        for i in range(len(whale_IDs_most_data)):
-            indexes = np.where(list_ids_arr == whale_IDs_most_data[i])[0]
-            idx_whale_IDs_most_data.extend(list(indexes))
-        idx_whale_IDs_most_data.sort()
-
-        # find image names corresponding to these whales
-        file_names_sub = [file_names[i] for i in idx_whale_IDs_most_data]
-        labels_int_sub = [labels_int[i] for i in idx_whale_IDs_most_data]
-        # should this be refit??
-        labels_int_sub_refit = list(le.fit_transform(labels_int_sub))  # refit it to the N classes
-
-        # labels_sub = [list_ids[i] for i in idx_whale_IDs_most_data]
-        # labels_sub == whale_IDs_most_data
-
-        # Parameters for Generator
-        partition = gen_imageName_dict(test_dir, train_dir, 0.2, file_names_sub)
-        imageName_ID_dict = dict(zip(file_names_sub, labels_int_sub_refit))
-        n_classes = N
-    else:
-        # Parameters for Generator
-        partition = gen_imageName_dict(test_dir, train_dir, 0.2)
-        imageName_ID_dict = dict(zip(file_names, labels_int))
-        n_classes = len(le.classes_)
-
-    # Parameters for Generator [OLD, BEFORE THE TRAIN TOP WHALES]
-    #partition = gen_imageName_dict(test_dir, train_dir, validation_fraction=0.2)
-    #labels = dict(zip(file_names, labels_int))
-
-    params = {'dim': (250, 500),
-              'batch_size': 32,
-              'n_classes': n_classes,
-              'n_channels': 1,
-              'shuffle': True}
-
-
-    training_generator = SiameseDataGenerator(list_IDs = partition['train'],
-                                              labels = imageName_ID_dict,
-                                              parent_dir = parent_dir,
-                                              **params)
-    validation_generator = SiameseDataGenerator(list_IDs = partition['validation'],
-                                                labels = imageName_ID_dict,
-                                                parent_dir=parent_dir,
-                                                **params)
 
     # Saving callback
     filepath = os.path.join(parent_dir, 'weights.best.basicSiamese.hdf5')
@@ -111,8 +105,7 @@ if __name__ == "__main__":
     # Model generation
     model = basicSiameseGenerator(parent_dir = parent_dir)
     history = model.fit_generator(generator=training_generator,
-                                  validation_data = validation_generator,
-                                  use_multiprocessing=True,
+                                  use_multiprocessing=False,
                                   epochs= N_EPOCHS,
                                   verbose=1,
                                   callbacks=callbacks_list)
